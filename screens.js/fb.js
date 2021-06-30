592f212a4b1cbd238edf3026ec0c6fb3f7d2227f
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-06-30 16:46:05 +0530

    Update fb.js

--- screens.js/fb.js
+++ screens.js/fb.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {Text,View} from 'react-native'
 
-export default class In extends React.Component{
+export default class fb extends React.Component{
     render(){
 
         return(
